418faf924c128f5d984dddde6229935ba8d343b9
-akku- <151517253+akku1139@users.noreply.github.com>
2025-03-04 13:39:06 +0900

    effect

--- src/signal.ts
+++ src/signal.ts
@@ -1,11 +1,21 @@
+export type Subscriber = () => void
+
+const gSubscribers: Array<Subscriber> = []
+
 export const signal = <T>(
   init: T, equals: (before: T, after: T) => boolean = (b, a) => b === a
-): { value: T, subscribe: (fn: Function) => void } => {
+): { value: T, subscribe: (fn: Subscriber) => void } => {
   let data: T = init
   const subscribers: Array<Function> = []
 
   return {
-    get value() { return data },
+    get value() {
+      const s = gSubscribers.at(-1)
+      if(s) {
+        this.subscribe(s)
+      }
+      return data
+    },
     set value(newValue) {
       if(equals(data, newValue)) {
         return
@@ -15,8 +25,14 @@ export const signal = <T>(
         s()
       }
     },
-    subscribe: (fn: Function) => {
+    subscribe: (fn) => {
       subscribers.push(fn)
     }
   }
+}
+
+export const effect = (fn: Subscriber): void => {
+  gSubscribers.push(fn)
+  fn()
+  gSubscribers.pop()
 }
