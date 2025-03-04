2f6f70f2225a63ffef2ba38233555d00ca85e19f
-akku- <151517253+akku1139@users.noreply.github.com>
2025-03-04 13:11:52 +0900

    Make props optional (use type assertion)

--- src/tagFactory.ts
+++ src/tagFactory.ts
@@ -1,12 +1,28 @@
 import type { Children } from "./types.ts"
 
-export const tagFactory = <T extends keyof HTMLElementTagNameMap>(tagName: T) =>
-  (props: Record<string, string>, ...children: Children): HTMLElementTagNameMap[T] => {
+type Tag<T extends keyof HTMLElementTagNameMap> = {
+  (props: Record<string, string>, ...children: Children): HTMLElementTagNameMap[T]
+  (...children: Children): HTMLElementTagNameMap[T]
+}
+
+export const tagFactory = <T extends keyof HTMLElementTagNameMap>(tagName: T): Tag<T> =>
+  (...args) => {
     const element = document.createElement<typeof tagName>(tagName)
-    Object.entries(props).forEach(([name, value]) => element.setAttribute(name, value))
+
+    let children: Children = []
+
+    if(!(args[0] instanceof HTMLElement)) {
+      const props = args[0]
+      Object.entries(props).forEach(([name, value]) => element.setAttribute(name, value))
+      children = args.slice(1) as Children
+    } else {
+      children = args as Children
+    }
+
     for(const child of children) {
       element.appendChild(child)
     }
+
     return element
   }
 
